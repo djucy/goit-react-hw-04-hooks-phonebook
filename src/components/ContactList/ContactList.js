7b90c571f2d862ccb5e3a7cadaff8aa46f7c55b3
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-04-23 15:40:15 +0300

    added changes to ContactList.module.css

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -11,17 +11,38 @@ import s from './ContactList.module.css';
 import PropTypes, { arrayOf } from 'prop-types';
 
 const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul className={ s.list}>
+  // <ul className={ s.list}>
+  //   {contacts.map(({ name, number, id }) => (
+  //     <li key={id} className ={s.item}>
+  //       <span className={s.name }>{name}:</span>
+  //       <span>{number}</span>
+  //       <button type="button" className={ s.button__delete} onClick={() => onDeleteContact(id)}>
+  //         Delete contact
+  //       </button>
+  //     </li>
+  //   ))}
+  // </ul>
+  <table className={s.table}>
+   <thead>
+    <tr>
+      <th className={s.item }>Name</th>
+      <th className={s.item }>Number</th>
+      <th></th>
+      </tr>
+    </thead>
+    <tbody>
     {contacts.map(({ name, number, id }) => (
-      <li key={id} className ={s.item}>
-        <span className={s.name }>{name}:</span>
-        <span>{number}</span>
-        <button type="button" className={ s.button__delete} onClick={() => onDeleteContact(id)}>
-          Delete contact
-        </button>
-      </li>
-    ))}
-  </ul>
+      <tr key={id} className={s.item }>
+        <td className ={s.table__date}>{ name}</td>
+        <td className ={s.table__date}>{number}</td>
+        <td className ={s.table__date}><button type="button" className={ s.button__delete} onClick={() => onDeleteContact(id)}>
+           Delete contact
+        </button></td>
+        
+    </tr>
+    )
+    )}</tbody>
+  </table>
 );
 export default ContactList;
 ContactList.propTypes = {
